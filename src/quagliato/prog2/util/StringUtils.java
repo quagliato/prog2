fa8f4d582472dc441641be40547419dbcfc85c25
Eduardo Quagliato <eduardo.quagliato@gmail.com>
2012-05-05 18:04:01 -0300

    Implementando o parâmetro side no método fillWithSpaces.

--- src/quagliato/prog2/util/StringUtils.java
+++ src/quagliato/prog2/util/StringUtils.java
@@ -5,6 +5,9 @@ package quagliato.prog2.util;
  * @author eduardo
  */
 public class StringUtils {
+    
+    public static final int LEFT = 1;
+    public static final int RIGHT = 2;
 
     public static String repeat(char c, int size) {
         if (size == 0) 
@@ -17,11 +20,16 @@ public class StringUtils {
         return result;
     }
     
-    public static String fillWithSpaces(String s, int size) {
+    public static String fillWithSpaces(String s, int size, int side) {
         if (size == 0 || size < s.length()) 
             return s;
         
-        String result = s + StringUtils.repeat(' ', size - s.length());
+        String result = "";
+        
+        if (StringUtils.LEFT == side)
+            result = StringUtils.repeat(' ', size - s.length()) + s;
+        else if (StringUtils.RIGHT == side)
+            result = s + StringUtils.repeat(' ', size - s.length());
         
         return result;
     }
