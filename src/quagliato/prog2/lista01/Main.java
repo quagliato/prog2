917d2d3e2fc4aa739039946052582433d23ccc0d
Eduardo Quagliato <eduardo.quagliato@gmail.com>
2012-05-14 23:59:21 -0300

    Alterando método de execução dos exercícios.

--- src/quagliato/prog2/lista01/Main.java
+++ src/quagliato/prog2/lista01/Main.java
@@ -1,6 +1,11 @@
 package quagliato.prog2.lista01;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import quagliato.prog2.interfaces.Exercicio;
 import quagliato.prog2.lista01.exe01.ExTesteHorario;
 import quagliato.prog2.lista01.exe03.ExTestadorDeCaixa;
 import quagliato.prog2.lista01.exe07.ExTesteCarro;
@@ -18,40 +23,40 @@ import quagliato.prog2.util.StringUtils;
 public class Main {
 
     public static void main(String args[]) {
+
+        Map<Integer, Class> exercicios = new HashMap<Integer, Class>();
+        exercicios.put(1, ExTesteHorario.class);
+        exercicios.put(3, ExTestadorDeCaixa.class);
+        exercicios.put(7, ExTesteCarro.class);
+        exercicios.put(9, ExTesteColaborador.class);
+        exercicios.put(14, ExTesteEmail.class);
+        exercicios.put(17, Principal17.class);
+        exercicios.put(19, Principal19.class);
+        exercicios.put(22, Principal22.class);
+
         boolean exit = false;
 
         while (!exit) {
             System.out.print("Escolha o exercicio da lista 1 (0 para sair): ");
             Scanner s = new Scanner(System.in);
             int a = s.nextInt();
-            switch (a) {
-                case 0:
-                    exit = true;
-                    break;
-                case 1:
-                    (new ExTesteHorario()).execute();
-                    break;
-                case 3:
-                    (new ExTestadorDeCaixa()).execute();
-                    break;
-                case 7:
-                    (new ExTesteCarro()).execute();
-                    break;
-                case 9:
-                    (new ExTesteColaborador()).execute();
-                    break;
-                case 14:
-                    (new ExTesteEmail()).execute();
-                    break;
-                case 17:
-                    (new Principal17()).execute();
-                    break;
-                case 19:
-                    (new Principal19()).execute();
-                    break;
-                case 22:
-                    (new Principal22()).execute();
-                    break;
+
+            if (a == 0) {
+                exit = true;
+            } else {
+                if (exercicios.containsKey(a)) {
+                    try {
+                        ((Exercicio) exercicios.get(a).newInstance()).execute();
+                    } catch (InstantiationException ex) {
+                        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IllegalAccessException ex) {
+                        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                } else {
+                    System.out.println("Exercício ainda não realizado.");
+                }
+                
+
             }
 
             System.out.println(StringUtils.repeat('+', 80));
